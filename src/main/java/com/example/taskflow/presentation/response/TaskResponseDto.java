7d4a92d5cbd298d360cf38bf00cfa8f66d0fac92
Madapada-TarunKumar-Git <tarunkumar.madapada@gmail.com>
2026-06-09 16:02:35 +0530

    add swagger ui schemas

--- src/main/java/com/example/taskflow/presentation/response/TaskResponseDto.java
+++ src/main/java/com/example/taskflow/presentation/response/TaskResponseDto.java
@@ -3,18 +3,39 @@ package com.example.taskflow.presentation.response;
 import com.example.taskflow.domain.enums.TaskPriority;
 import com.example.taskflow.domain.enums.TaskStatus;
 import com.example.taskflow.domain.enums.TaskType;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.time.Instant;
 
+@Schema(name = "Task response payload")
 public record TaskResponseDto(
+        @Schema(example = "1")
         Long id,
+
+        @Schema(example = "Customer Import")
         String taskName,
+
+        @Schema(example = "customer details....")
         String description,
+
+        @Schema(example = "CUSTOMER_IMPORT")
         TaskType taskType,
+
+        @Schema(example = "QUEUED")
         TaskStatus status,
+
+        @Schema(example = "HIGH")
         TaskPriority priority,
+
+        @Schema(example = "1")
         int retryCount,
+
+        @Schema(example = "System")
         String createdBy,
-        Instant createdAt
+
+        Instant createdAt,
+        Instant processingStartedAt,
+        Instant processingCompletedAt,
+        Long processingDurationMs
 ) {
 }
