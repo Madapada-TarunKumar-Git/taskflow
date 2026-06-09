cc07731976976b8b58f7f0aa51d274f9e426149f
Madapada-TarunKumar-Git <tarunkumar.madapada@gmail.com>
2026-06-09 15:52:54 +0530

    add count by status method and query

--- src/main/java/com/example/taskflow/infrastructure/persistence/repository/JpaTaskAuditRepository.java
+++ src/main/java/com/example/taskflow/infrastructure/persistence/repository/JpaTaskAuditRepository.java
@@ -2,9 +2,12 @@ package com.example.taskflow.infrastructure.persistence.repository;
 
 import com.example.taskflow.infrastructure.persistence.entity.TaskAuditEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface JpaTaskAuditRepository extends JpaRepository<TaskAuditEntity, Long> {
+
+    @Query("SELECT tae FROM TaskAuditEntity tae WHERE tae.taskId = :taskId")
     List<TaskAuditEntity> findByTaskIdOrderByCreatedAtAsc(Long taskId);
 }
