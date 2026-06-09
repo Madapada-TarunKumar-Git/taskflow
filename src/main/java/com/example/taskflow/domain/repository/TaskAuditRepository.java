bac1d9384c81173562e12be1aa792bedc08f500e
Madapada-TarunKumar-Git <tarunkumar.madapada@gmail.com>
2026-06-09 15:57:48 +0530

    update for better readability

--- src/main/java/com/example/taskflow/domain/repository/TaskAuditRepository.java
+++ src/main/java/com/example/taskflow/domain/repository/TaskAuditRepository.java
@@ -1,10 +1,12 @@
 package com.example.taskflow.domain.repository;
 
 import com.example.taskflow.domain.model.TaskAudit;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface TaskAuditRepository {
     TaskAudit save(TaskAudit taskAudit);
+
     List<TaskAudit> findByTaskId(Long taskId);
 }
