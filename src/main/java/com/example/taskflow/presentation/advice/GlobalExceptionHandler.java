12c08fef6573a5d814685baf4398076dac7ade28
Madapada-TarunKumar-Git <tarunkumar.madapada@gmail.com>
2026-06-11 21:44:09 +0530

    add JWT dependencies for enhanced authentication support

--- src/main/java/com/example/taskflow/presentation/advice/GlobalExceptionHandler.java
+++ src/main/java/com/example/taskflow/presentation/advice/GlobalExceptionHandler.java
@@ -3,11 +3,15 @@ package com.example.taskflow.presentation.advice;
 import com.example.taskflow.domain.exception.InvalidTaskStateException;
 import com.example.taskflow.domain.exception.MaxRetryLimitExceededException;
 import com.example.taskflow.domain.exception.TaskNotFoundException;
+import io.jsonwebtoken.ExpiredJwtException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
+import org.springframework.security.authorization.AuthorizationDeniedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 import java.net.URI;
 import java.time.Instant;
@@ -83,6 +87,46 @@ public class GlobalExceptionHandler {
         problemDetail.setTitle("Invalid/Bad Request");
         problemDetail.setType(URI.create("/errors/bad-request"));
         problemDetail.setProperty("timestamp", Instant.now());
+
+        return problemDetail;
+    }
+
+    @ExceptionHandler(AuthorizationDeniedException.class)
+    public ProblemDetail handleAuthorizationDeniedException(AuthorizationDeniedException ex){
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.FORBIDDEN, ex.getMessage());
+        problemDetail.setTitle("Authorization Denied");
+        problemDetail.setType(URI.create("/errors/forbidden"));
+        problemDetail.setProperty("timestamp", Instant.now());
+
+        return problemDetail;
+    }
+
+    @ExceptionHandler(ExpiredJwtException.class)
+    public ProblemDetail handleExpiredJwtException(ExpiredJwtException ex){
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.UNAUTHORIZED, ex.getMessage());
+        problemDetail.setTitle("Expired JWT Token");
+        problemDetail.setType(URI.create("/errors/expired-token"));
+        problemDetail.setProperty("timestamp", Instant.now());
+
+        return problemDetail;
+    }
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public ProblemDetail handleNoHandlerFoundException(NoHandlerFoundException ex){
+        ProblemDetail problemDetail = ProblemDetail.forStatus(HttpStatus.NOT_FOUND);
+        problemDetail.setDetail("Invalid API endpoint: " + ex.getRequestURL());
+        problemDetail.setType(URI.create("/errors/not-found"));
+        problemDetail.setProperty("timestamp", Instant.now());
+
+        return problemDetail;
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ProblemDetail handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException ex){
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, ex.getMessage());
+        problemDetail.setTitle("Bad Request");
+        problemDetail.setType(URI.create("/errors/bad-request"));
+        problemDetail.setProperty("timestamp",Instant.now());
 
         return problemDetail;
     }
