69f6b65bd19e11eac1304df16bec0b94288fe6fd
Madapada-TarunKumar-Git <tarunkumar.madapada@gmail.com>
2026-06-20 15:25:21 +0530

    refactor to register and login user end points and proper request input validation

--- src/main/java/com/example/taskflow/presentation/request/AuthRequest.java
+++ src/main/java/com/example/taskflow/presentation/request/AuthRequest.java
@@ -1,12 +1,14 @@
 package com.example.taskflow.presentation.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public record AuthRequest(
         @NotBlank(message = "Username is required")
         String username,
 
         @NotBlank(message = "Password is required")
+        @Size(min = 8, max = 16, message = "Password length must be 8 to 16 characters")
         String password
 ) {
 }
